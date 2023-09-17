4f3edacd622c014e57b3f42e54bfa096371c10a9
Gustavo Machado <gustavomf02@gmail.com>
2023-09-17 11:04:04 -0300

    Padroniza url dos endpoints

--- src/main/java/com/example/emonitorback/controller/TicketsController.java
+++ src/main/java/com/example/emonitorback/controller/TicketsController.java
@@ -10,13 +10,11 @@ import com.example.emonitorback.service.MessageService;
 import com.example.emonitorback.service.TicketService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RequestMapping("/api/v1/tickets")
 @RestController
 @Tag(name = "Tickets Controller", description = "Endpoints para gerenciamento de tickets. " +
         "O usuário precisa estar autenticado para acessar.")
